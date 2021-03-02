43f89dc046b10fb26a987b7e3649f28264da4fef
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-03-02 16:46:56 +0300

    Add project files.

---
+++ ShopApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
