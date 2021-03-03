3cfde5953bf29763bd428af526e3bd6aae62bd52
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-03-03 20:53:16 +0300

    Views

--- ShopApplication/Controllers/HomeController.cs
+++ ShopApplication/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace ShopApplication.Controllers
         {
             return View();
         }
+        public IActionResult productDetail()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
