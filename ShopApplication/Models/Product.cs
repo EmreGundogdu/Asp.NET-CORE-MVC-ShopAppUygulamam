d05835df89cb28409927709a358cd444d2ddbcfb
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-03-10 20:05:00 +0300

    Added categoryId prop to product

--- ShopApplication/Models/Product.cs
+++ ShopApplication/Models/Product.cs
@@ -2,6 +2,7 @@ namespace ShopApplication.Models
 {
     public class Product
     {
+        public int CategoryId { get; set; }
         public int ProductId { get; set; }
         public string Name { get; set; }        
         public double Price { get; set; }        
