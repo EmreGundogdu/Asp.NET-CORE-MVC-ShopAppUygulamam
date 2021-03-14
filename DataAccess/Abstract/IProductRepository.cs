68f65af94fef524e88f93d94e118778e32ee2a07
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-03-14 22:23:18 +0300

    DataAccess library folders and DatabaseRepositories are created

---
+++ DataAccess/Abstract/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductRepository
+    {
+        Product GetById(int id);
+        List<Product> GetAll();
+        void Create(Product entity);
+        void Update(Product entity);
+        void Delete(int id);
+    }
+}
