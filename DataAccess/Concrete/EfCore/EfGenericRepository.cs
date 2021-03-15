62aaa84d4c0f5250cdf08559a40d29167d901dd0
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-03-15 19:42:15 +0300

    GenericRepository class

---
+++ DataAccess/Concrete/EfCore/EfGenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EfCore
+{
+    public class EfGenericRepository<TEntity, TContext> : IRepository<TEntity> where TEntity : class where TContext : DbContext, new()
+    {
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
