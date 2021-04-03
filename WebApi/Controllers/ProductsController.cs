b15818d6cc1c847e0aa90c97eeaf4c9216f3ecc4
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-04-03 10:41:42 +0300

    Product controller

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Products =
+        {
+            "samsung s8","samsung s10","iphone 12","iphone 12max"
+        };
+        public string[] GetProducts()
+        {
+            return Products;
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return Products[id];
+        }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -44,7 +44,7 @@ namespace WebApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
